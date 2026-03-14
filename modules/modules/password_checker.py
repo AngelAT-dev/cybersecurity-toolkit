64022dbad184d1d08535c8af58709abc9a8494d8
Angel Tolentino <angel.tolentino.dev@gmail.com>
2026-03-13 21:09:34 -0600

    Create password_checker.py

---
+++ modules/modules/password_checker.py
@@ -0,0 +1,17 @@
+import re
+
+def check_password(password):
+
+    length = len(password) >= 8
+    upper = re.search(r"[A-Z]", password)
+    lower = re.search(r"[a-z]", password)
+    digit = re.search(r"\d", password)
+
+    score = sum([bool(length), bool(upper), bool(lower), bool(digit)])
+
+    if score == 4:
+        print("Strong password")
+    elif score >= 2:
+        print("Moderate password")
+    else:
+        print("Weak password")
