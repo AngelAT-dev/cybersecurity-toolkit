137822f8606a8b79fba8eecb73c82952ca08f0e4
Angel Tolentino <angel.tolentino.dev@gmail.com>
2026-03-13 21:09:15 -0600

    Create port_scanner.py

---
+++ modules/port_scanner.py
@@ -0,0 +1,15 @@
+import socket
+
+def port_scan(target):
+    print(f"\nScanning {target}...\n")
+
+    for port in range(1, 1025):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.5)
+
+        result = s.connect_ex((target, port))
+
+        if result == 0:
+            print(f"Port {port} is OPEN")
+
+        s.close()
