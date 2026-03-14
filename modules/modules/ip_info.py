fb7aa995ae0f752cc21dfdcebcbebc6ddf6befc3
Angel Tolentino <angel.tolentino.dev@gmail.com>
2026-03-13 21:10:45 -0600

    Create ip_info.py

---
+++ modules/modules/ip_info.py
@@ -0,0 +1,15 @@
+import socket
+
+def get_ip_info(target):
+
+    try:
+        ip = socket.gethostbyname(target)
+
+        print("IP Address:", ip)
+
+        host = socket.gethostbyaddr(ip)
+
+        print("Hostname:", host[0])
+
+    except socket.error:
+        print("Unable to retrieve information")
